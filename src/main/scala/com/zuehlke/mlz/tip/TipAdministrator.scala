368ddf8d364a652c0a1d97bf7d3a3289d611cb9e
Magdalena <mlz@zuehlke.com>
2014-06-24 17:15:42 +0200

    removing unused files

--- src/main/scala/com/zuehlke/mlz/tip/TipAdministrator.scala
+++
@@ -1,6 +0,0 @@
-package com.zuehlke.mlz.tip
-
-
-class TipAdministrator {
-
-}
--- src/test/scala/com/zuehlke/mlz/kata/pacman/FieldGeneratorSpec.scala
+++
@@ -1,6 +0,0 @@
-package com.zuehlke.mlz.kata.pacman
-
-import akka.testkit.TestKit
-import akka.actor.ActorSystem
-
-class FieldGeneratorSpec extends TestKit(ActorSystem("Pacman-test"))
